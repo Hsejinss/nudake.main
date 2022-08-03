31f91a3548f32222d807aabcd36ff87b891aa47a
Sejin Han <84485920+Hsejinss@users.noreply.github.com>
2022-08-03 11:03:51 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   AOS.init(); //aos 호출
 
+  // 햄버거버튼 클릭시 active
+  $("#hamburger").click(function () {
+    $(this).toggleClass("active");
+    $(".nav-area").toggleClass("active");
+  });
   // sec-2 영역, 번호 누르면 left 이미지 변경, right 설명 변경
   $(".sec-2 .right-content li").click(function () {
     var detail = $(this).attr("data-alt");
@@ -21,6 +26,12 @@ $(document).ready(function () {
     $(".sec-2 .left-content").css({
       "background-image": `url(${changeBg})`,
     });
+  });
+
+  // 번호 클릭시 li에 active
+  $(".sec-2 .right-content .new-num li").click(function () {
+    $(".sec-2 .right-content .new-num li a").addClass("active");
+    $(this).siblings().children().removeClass("active");
   });
 
   // sec-1에 닿을때 line svg가 그려지도록
